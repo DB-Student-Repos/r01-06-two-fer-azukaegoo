5ae918f6e16f476337d7316238ddebccf27c31aa
Gloria Azum <azumgloria@gmail.com>
2024-03-16 18:35:55 +0100

    removed fer_two

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub fn twofer(name: &str) -> String {
 
-    let fer_two = match name {
+    match name {
         "" => String::from("One for you, one for me."),
         _ => format!("One for {}, one for me.", name),
-    };
-    fer_two
+    }
+
 }
